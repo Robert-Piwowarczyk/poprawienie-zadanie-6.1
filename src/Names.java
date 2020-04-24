a3e662ec5dfa9a2f20f45b0222d384887ef80865
HelloWorld <robpiw@op.pl>
2020-04-24 10:31:57 +0200

    Odwrócone powitania

---
+++ src/Names.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Names {
+
+    public static void main(String[] args) {
+        String[] firstNames = new String[5];
+        for (int a = 0;a<1; a++){int b = 1;int c = 2;int d = 3;int e = 4;
+            System.out.println("podaj 5 imion:");
+            Scanner input = new Scanner(System.in);
+            firstNames[a] = input.nextLine();
+            firstNames[b] = input.nextLine();
+            firstNames[c] = input.nextLine();
+            firstNames[d] = input.nextLine();
+            firstNames[e] = input.nextLine();
+                System.out.println("Cześć " + firstNames[e]);
+                System.out.println("Cześć " + firstNames[d]);
+                System.out.println("Cześć " + firstNames[c]);
+                System.out.println("Cześć " + firstNames[b]);
+                System.out.println("Cześć " + firstNames[a]);
+                }
+            }
+        }
+
+
+
+
+
